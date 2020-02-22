5cc548d3c53785b2c84c2d54eba14b4e9ea1f498
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-02-22 16:28:32 -0700

    Update Fraction.h

--- src/Fraction.h
+++ src/Fraction.h
@@ -13,29 +13,29 @@ class Fraction
 private:
   int _num;
   int _den;
-  int gcd (int first, int second);
-  int lcm (int first, int second);
-  Fraction apply_lcm (int lcm, Fraction const & rhs, Fraction const & lhs);
-  std::vector <int> comparison_test (int lcm, Fraction const & rhs, Fraction const & lhs);
+  int gcd (int, int);
+  int lcm (int, int);
+  Fraction apply_lcm (int, Fraction const &, Fraction const &);
+  std::vector <int> comparison_test (int, Fraction const &, Fraction const &);
 public:
-  Fraction (int top = 0, int bottom = 1);
-  Fraction (int top = 0);
+  Fraction (int, int);
+  Fraction (int);
   Fraction operator - ();
-  Fraction operator + (Fraction const & rhs);
-  Fraction operator - (Fraction & rhs);
-  Fraction operator * (Fraction const & rhs);
-  Fraction operator / (Fraction const & rhs);
-  bool operator == (Fraction const & rhs);
-  bool operator != (Fraction const & rhs);
-  bool operator < (Fraction const & rhs);
-  bool operator > (Fraction const & rhs);
-  bool operator <= (Fraction const & rhs);
-  bool operator >= (Fraction const & rhs);
+  Fraction operator + (Fraction const &);
+  Fraction operator - (Fraction &);
+  Fraction operator * (Fraction const &);
+  Fraction operator / (Fraction const & );
+  bool operator == (Fraction const & );
+  bool operator != (Fraction const & );
+  bool operator < (Fraction const & );
+  bool operator > (Fraction const & );
+  bool operator <= (Fraction const & );
+  bool operator >= (Fraction const & );
   ~ Fraction ();
   float toDecimal () const;
   void print ();
-  friend std::ostream & operator << (std::ostream & os, Fraction fraction);
+  friend std::ostream & operator << (std::ostream & , Fraction );
 };
-std::ostream & operator << (std::ostream & os, Fraction fraction);
+std::ostream & operator << (std::ostream & , Fraction );
 #undef LZZ_INLINE
 #endif
